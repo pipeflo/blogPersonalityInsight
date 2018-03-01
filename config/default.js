2194cca33a5445edc942a09c3b3802d93772ed5c
Luis Florez <lflorez@co.ibm.com>
2018-03-01 06:56:34 -0600

    Adding extension file

--- config/default.js
+++ config/default.js
@@ -4,19 +4,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-	AppConfig: {
-		domain: "https://localhost:3000"
-	},
-
 	ConnectionsConfig: {
 		hostname: 'apps.na.collabserv.com',
 	  clientID: '<ClientID>',
 	  clientSecret: '<ClientSecret>',
 	  callbackURL: 'https://localhost:3000/callback', //https is important here. Connections Cloud doesn't accept http callback urls
+		appDomain: "https://localhost:3000"
 	},
 
 	PersonalityInsights: {
-		username: "",
-		password: ""
+		username: "<PI-Username>",
+		password: "<PI-Password>"
 	}
 }
--- config/production.js
+++ config/production.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
 
 	PersonalityInsights: {
-		user: "",
+		username: "",
 		password: ""
 	}
 }
